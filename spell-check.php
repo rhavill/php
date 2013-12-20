995b26801f7ad40d3e756470fa5b75dd2e68e507
Ricardo Havill <rhavill@progresstesting.com>
2013-12-20 16:07:48 -0500

    Modified spell-checking test script.

--- spell-check.php
+++ spell-check.php
@@ -1,4 +1,32 @@
 <?php
 
+class SpellChecker {
+    private $dictionary;
+    
+    public function __construct() {
+        $this->dictionary = pspell_new('en','','','',PSPELL_FAST);
+    }
+    
+    private function getWords($text) {
+        //$words = str_word_count($text, 1, 'àáé');
+        $words = str_word_count($text, 1);
+        return $words;
+    }
+    
+    public function getMisspelledWords($text) {
+        $misspelled = array();
+        $words = $this->getWords($text);
+        foreach ($words as $word) {
+            if (!pspell_check($this->dictionary, $word)) {
+                $misspelled[] = $word;
+            }
+        }
+        return $misspelled;
+    }
+}
+//$speller = new SpellChecker();
+//$text = 'Here are some words. This word is missspelled.';
+//print_r($speller->getMisspelledWords($text));
 $pspell_link = pspell_new('en');
-var_dump(pspell_check($pspell_link, 'this is a test'));
+//var_dump(pspell_check($pspell_link, 'càt'));
+var_dump(pspell_check($pspell_link, 'c'));
