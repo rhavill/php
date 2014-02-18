744b9370d4e20866935230467a4ddb5e5ea1bbec
Ricardo Havill <rhavill@progresstesting.com>
2014-02-18 12:21:27 -0500

    Modified spell-checking script to allow some special characters to be considered as word characters when parsing words.

--- spell-check.php
+++ spell-check.php
@@ -2,14 +2,14 @@
 
 class SpellChecker {
     private $dictionary;
-    
+    private $extraAllowedChars = 'ÀÁÂÃÄÅÆàáâãäåæÇçÐÈÉÊËèéêëÌÍÎÏìíîïÑñÒÓÔÕÖØŒòóôõöøœðÞþßÙÚÛÜùúûüŴŵÝŶŷýÿ';
     public function __construct() {
         $this->dictionary = pspell_new('en','','','',PSPELL_FAST);
     }
     
-    private function getWords($text) {
+    public function getWords($text) {
         //$words = str_word_count($text, 1, 'àáé');
-        $words = str_word_count($text, 1);
+        $words = str_word_count($text, 1, $this->extraAllowedChars);
         return $words;
     }
     
@@ -24,8 +24,20 @@ class SpellChecker {
         return $misspelled;
     }
 }
+
+function removeTags($text) {
+    return preg_replace('/<[^>]+>/', ' ', $text);
+}
+
 $speller = new SpellChecker();
-$html = 'I am a great speler. How do you spell ag&agrave;ve? How to spell càt?  How to spell r&eacute;sum&eacute;? Is this mispelled? You like piña coladas?';
+$html = 'I can\'t spell good. I am a great speler. How do you spell ag&agrave;ve? How to spell càt?  How to spell r&eacute;sum&eacute;? Is this mispelled? You like piña coladas?';
+$html .= '<p>first paragraph contains a misspelled wurd</p><p>second paragraph lso has mispeled word.</p><p>The word r&eacute;sum&eacute; has html entities.</p><img src="blah.jpg"/>';
 $text = html_entity_decode($html);
-print "$text\n";
+//$text = $html;
+print "Text: $text\n";
+$text = removeTags($text);
+print "Clean Text: $text\n";
+print "Words:\n";
+print_r($speller->getWords($text));
+print "Misspelled:\n";
 print_r($speller->getMisspelledWords($text));
