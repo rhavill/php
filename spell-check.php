2fa812a9afa58e2e7ce9bb037a9a7d7fa1b163a1
Ricardo Havill <rhavill@progresstesting.com>
2014-03-05 14:15:00 -0500

    Updated SpellChecker class.

--- spell-check.php
+++ spell-check.php
@@ -2,26 +2,80 @@
 
 class SpellChecker {
     private $dictionary;
+    private $text;
+    private $cleanedText;
     private $extraAllowedChars = 'ÀÁÂÃÄÅÆàáâãäåæÇçÐÈÉÊËèéêëÌÍÎÏìíîïÑñÒÓÔÕÖØŒòóôõöøœðÞþßÙÚÛÜùúûüŴŵÝŶŷýÿ';
-    public function __construct() {
+    
+    private function removeTags($text) {
+        return preg_replace('/<[^>]+>/', ' ', $text);
+    }
+    
+    // This function was copied from http://php.net/manual/en/function.str-word-count.php
+//    private function str_word_count_utf8($str) {
+//        preg_match_all("/\\p{L}[\\p{L}\\p{Mn}\\p{Pd}'\\x{2019}]*/u", $str, $matches);
+//        return $matches[0];
+//    } 
+    
+    public function __construct($text = '') {
         $this->dictionary = pspell_new('en','','','',PSPELL_FAST);
+        if ($text) {
+            $this->setText($text);
+        }
+    }
+    
+    public function setText($text) {
+        $this->text = $text;
+        $this->cleanedText = html_entity_decode($this->removeTags($text));
+        // Remove MathML
+        $this->cleanedText = preg_replace('/`[^`]+`/', '', $this->cleanedText);        
     }
     
     public function getWords($text) {
-        //$words = str_word_count($text, 1, 'àáé');
-        $words = str_word_count($text, 1, $this->extraAllowedChars);
+        $words = array();
+        $possibleWords = str_word_count($text, 1, $this->extraAllowedChars);
+        //$possibleWords = $this->str_word_count_utf8($this->cleanedText, 1, $this->extraAllowedChars);
+        for ($i = 0; $i < count($possibleWords); $i++) {
+            // Treat hyphenated words as 2 words
+            if (strpos($possibleWords[$i], '-') !== FALSE) {
+                $splitWords = split("-", $possibleWords[$i]);
+                foreach ($splitWords as $splitWord) {
+                    $words[] = $splitWord;
+                }
+            }
+            else {
+                $words[] = $possibleWords[$i];
+            }
+        }
         return $words;
     }
     
-    public function getMisspelledWords($text) {
+    public function getMisspelledWords() {
         $misspelled = array();
-        $words = $this->getWords($text);
+        $words = $this->getWords($this->cleanedText);
         foreach ($words as $word) {
-            if (!pspell_check($this->dictionary, $word)) {
+            // Ignore acronyms or words with all capital letters
+            if ($word == strtoupper($word)) {
+                continue;
+            }
+            if (!pspell_check($this->dictionary, $word) && !in_array($word, $misspelled)) {
                 $misspelled[] = $word;
             }
         }
         return $misspelled;
+    }
+    
+    public function highlightMisspelledWords($misspelledWords = array()) {
+        if (empty($misspelledWords)) {
+            $misspelledWords = $this->getMisspelledWords($this->cleanedText);
+        }
+        $text = $this->text;
+        foreach ($misspelledWords as $word) {
+            $text = str_replace($word, "<span class=\"misspelled\">$word</span>", $text);
+            if ($word != htmlentities($word)) {
+                $text = str_replace(htmlentities($word), "<span class=\"misspelled\">$word</span>", $text);
+            }
+        }
+        return $text;
     }
 }
 
