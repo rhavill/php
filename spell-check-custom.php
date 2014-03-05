86531941d6c2a6e65b5b3de73ad658a030ad2542
Ricardo Havill <rhavill@progresstesting.com>
2014-03-05 14:25:38 -0500

    Fixed bug in spell-checker with custom dictionary by changing order of function calls.

--- spell-check-custom.php
+++ spell-check-custom.php
@@ -3,10 +3,10 @@
 $p = pspell_new("en","","","",PSPELL_FAST);
 
 $pspell_config = pspell_config_create("en");
-$pspell_link = pspell_new_config($pspell_config);
-var_dump(pspell_check($pspell_link,"swagg"));
-
 pspell_config_personal($pspell_config, "/tmp/custom.pws");
+$pspell_link = pspell_new_config($pspell_config);
+var_dump(pspell_check($pspell_link,"swagzilla"));
 
-pspell_add_to_personal($pspell_link, "swagg");
+
+pspell_add_to_personal($pspell_link, "swagzilla");
 pspell_save_wordlist($pspell_link);
