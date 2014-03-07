3d16c1e868e9ec54243b903b1e072c85eabe6628
Ricardo Havill <rhavill@progresstesting.com>
2014-03-07 11:21:08 -0500

    Added spell-checking examples for enchant library, which seems to handle utf-8 better.

--- spell-check-custom.php
+++ spell-check-custom.php
@@ -1,10 +1,11 @@
 <?php
-
-$pspell_config = pspell_config_create("en");
+$word = html_entity_decode("r&eacute;s&eacute;");
+print "$word\n";
+$pspell_config = pspell_config_create("en",'american', '', 'utf-8');
 pspell_config_personal($pspell_config, "/tmp/custom.pws");
 $pspell_link = pspell_new_config($pspell_config);
-var_dump(pspell_check($pspell_link,"swagzilla"));
+//var_dump(pspell_check($pspell_link,$word));
 
 
-pspell_add_to_personal($pspell_link, "swagzilla");
+pspell_add_to_personal($pspell_link, $word);
 pspell_save_wordlist($pspell_link);
