07340c9fbecec1bd2d455db602dc65480a488966
Ricardo Havill <rhavill2002@yahoo.com>
2014-10-26 11:55:17 -0400

    Added example of coordinated products.

---
+++ patterns/factory-method/multiple-creators/coordinating-products/GraphicProduct.php
@@ -0,0 +1,13 @@
+<?php
+//GraphicProduct.php
+include_once('Product.php');
+
+class GraphicProduct implements Product {
+
+	private $mfgProduct;
+
+	public function getProperties() {
+		$this->mfgProduct='<img style="padding: 10px 10px 10px 0px;" src="Mali.png" align="left" width="256" height="274">';
+		return $this->mfgProduct;
+	}
+}
