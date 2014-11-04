b1c262ef3c81ae781fd1adc0bbb117f77bafab60
Ricardo Havill <rhavill2002@yahoo.com>
2014-11-03 23:58:38 -0500

    Added "create table" class to proxy pattern example.

---
+++ design-patterns/database/proxy-pattern/CreateTable.php
@@ -0,0 +1,25 @@
+<?php
+include_once('UniversalConnect.php');
+
+class CreateTable {
+	private $tableMaster;
+	private $hookup;
+
+	public function __construct() {
+		$this->tableMaster="proxyLog";
+		$this->hookup=UniversalConnect::doConnect();
+		$drop = "DROP TABLE IF EXISTS $this->tableMaster";
+		if($this->hookup->query($drop) === true) {
+			printf("Old table %s has been dropped.<br/>\n",$this->tableMaster);
+		}
+		$sql = "CREATE TABLE {$this->tableMaster} (uname NVARCHAR(15),
+		pw NVARCHAR(120))";
+		if($this->hookup->query($sql) === true) {
+			echo "Table $this->tableMaster has been created successfully.<br/>\n";
+		} else {
+			echo "Error: " . mysqli_error($this->hookup) . "<br/>\n";
+		}
+		$this->hookup->close();
+	}
+}
+$worker=new CreateTable();
