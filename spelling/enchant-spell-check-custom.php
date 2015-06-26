b1f7b1bf223fd830d0df46f3de0209fd3007b1cd
Ricardo Havill <rhavill@progresstesting.com>
2015-06-26 11:02:23 -0400

    Updated spell-checker example.

--- spelling/enchant-spell-check-custom.php
+++ spelling/enchant-spell-check-custom.php
@@ -6,6 +6,12 @@ $dictionary = enchant_broker_request_dict($broker, 'en_US');
 $customDictionary = enchant_broker_request_pwl_dict ($broker, '/tmp/enchant.pwl');
 enchant_dict_add_to_personal($customDictionary, 'résumé');
 foreach ($words as $word) {
-	$isCorrect = enchant_dict_check($customDictionary, $word);
+	$isCorrect = FALSE;
+	if (enchant_dict_check($dictionary, $word)) {
+       $isCorrect = TRUE;
+    }
+    else if ($customDictionary && enchant_dict_check($customDictionary, $word)) {
+        $isCorrect = TRUE;
+    }
 	print "$word is" . ($isCorrect ? '' : ' NOT') . " spelled correctly.\n";
 }
