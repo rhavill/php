d30c41844c9a70845e1018f2fec5271ddf6073e0
Ricardo Havill <rhavill2002@yahoo.com>
2019-10-27 17:46:34 -0400

    Allow image resizing script to be called from any directory.

--- resize.php
+++ resize.php
@@ -1,6 +1,15 @@
 <?php
 
-$files = scandir(".");
+$sourceDir = rtrim($argv[1], '/');
+$destDir = rtrim($argv[2], '/');
+if (!$destDir) {
+    print "This script must be provided with a source dir and a destination ".
+        "dir. Example: php resize.php source-dir dest-dir\n";
+    exit;
+}
+
+// chdir($sourceDir);
+$files = scandir($sourceDir);
 
 foreach ($files as $file) {
     if ($file == 'resize.php') {
@@ -9,18 +18,19 @@ foreach ($files as $file) {
     
     list($fileName, $extension) = explode('.', $file);
     if ($extension == 'jpg') {
-        $dest = "small/$file";
+        $src = "$sourceDir/$file";
+        $dest = "$destDir/$file";
         
-        list($width, $height) = getimagesize($file);
+        list($width, $height) = getimagesize($src);
         $newWidth = $width / 4;
         $newHeight = $height / 4;
 
-        $source = imagecreatefromjpeg($file);
+        $source = imagecreatefromjpeg($src);
         $newImage = imagecreatetruecolor($newWidth, $newHeight);
         imagecopyresampled($newImage, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
         imagejpeg($newImage, $dest, 100);
         imagedestroy($source);
         imagedestroy($newImage);
-        print "$file ($width x $height) -> $dest ($newWidth x $newHeight)\n\n";
+        print "$src ($width x $height) -> $dest ($newWidth x $newHeight)\n\n";
     }
 }
